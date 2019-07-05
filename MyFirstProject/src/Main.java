a9d9304e77c2e5eae0205e90832dedb02c9394e0
z4Zachariah <zacha@192.168.1.68>
2019-07-06 08:28:38 +1200

    update coment and master branch

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// outputs hello world
+		// output hello world and update master branch
 
 		System.out.println("Hello World");
 	}
